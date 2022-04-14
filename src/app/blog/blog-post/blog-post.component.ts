86c5d4d1123b9dd214ba4a03b6d899dcc636dcc2
ishafiul <shafiulislam20@gmail.com>
2022-04-14 15:41:27 +0600

    blog bost page

--- src/app/blog/blog-post/blog-post.component.ts
+++ src/app/blog/blog-post/blog-post.component.ts
@@ -10,6 +10,41 @@ export class BlogPostComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const fixedTarget = document.querySelector('#fixed-target')
+    const fixedTarget2 = document.querySelector('#fixed-target2')
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const target = entry.target
+        if (entry.isIntersecting) {
+          // @ts-ignore
+          fixedTarget.classList.remove('fixed')
+          // @ts-ignore
+          fixedTarget.classList.remove('-mt-[50rem]')
+
+          // @ts-ignore
+          fixedTarget2.classList.remove('fixed')
+          // @ts-ignore
+          fixedTarget2.classList.remove('-mt-[50rem]')
+        } else {
+          // @ts-ignore
+          fixedTarget.classList.add('fixed')
+          // @ts-ignore
+          fixedTarget.classList.add('-mt-[50rem]')
+          // @ts-ignore
+          // @ts-ignore
+
+
+          // @ts-ignore
+          fixedTarget2.classList.add('fixed')
+          // @ts-ignore
+          fixedTarget2.classList.add('-mt-[50rem]')
+          // @ts-ignore
+        }
+      })
+    })
+
+    // @ts-ignore
+    observer.observe(document.querySelector('#box'))
   }
 
 }
